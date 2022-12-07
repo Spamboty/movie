327f663a934d9959bc5031c020f7c46720a49f39
Spamboty <119392303+Spamboty@users.noreply.github.com>
2022-12-07 19:27:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ PICS = (getenv('PICS', 'https://telegra.ph//file/b12246b0780a927bc8190.jpg https
 # Admins, Channels & Users
 ADMINS = int(getenv('ADMINS', '5463205082').split())
 CHANNELS = int(getenv('CHANNELS', ' -1001863651025').split())
-auth_users = int(getenv('AUTH_USERS', '5463205082').split()]
+auth_users = int(getenv('AUTH_USERS', '5463205082').split()
 auth_channel = getenv('AUTH_CHANNEL', ' -1001863651025')
 auth_grp = getenv('AUTH_GROUP', '-1001666282080')
 
