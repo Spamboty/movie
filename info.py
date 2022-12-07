edf03009d59b99c19e07cd17360a4894400d5cb4
Spamboty <119392303+Spamboty@users.noreply.github.com>
2022-12-07 19:39:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,8 +22,8 @@ PICS = (getenv('PICS', 'https://telegra.ph//file/b12246b0780a927bc8190.jpg https
 ADMINS = int(getenv('ADMINS', '5463205082').split())
 CHANNELS = int(getenv('CHANNELS', ' -1001863651025').split())
 AUTH_USERS = int(getenv('AUTH_USERS', '5463205082').split()
-AUTH_CHANNEL = getenv('AUTH_CHANNEL', ' -1001863651025')
-AUTH_GROUP = getenv('AUTH_GROUP', '-1001666282080')
+AUTH_CHANNEL = int(getenv('AUTH_CHANNEL', ' -1001863651025')
+AUTH_GROUP = int(getenv('AUTH_GROUP', '-1001666282080')
 
 # MongoDB information
 DATABASE_URI = getenv('DATABASE_URI', "mongodb+srv://movies:7234049299@cluster0.mc1he3h.mongodb.net/?retryWrites=true&w=majority")
@@ -43,7 +43,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(getenv("LONG_IMDB_DESCRIPTION", "False"), Fal
 SPELL_CHECK_REPLY = is_enabled(getenv("SPELL_CHECK_REPLY", "True"), False)
 MAX_LIST_ELM = getenv.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(getenv('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = int(getenv('FILE_STORE_CHANNEL', '')).split()
+FILE_STORE_CHANNEL = int(getenv('FILE_STORE_CHANNEL', '-1001682125120')).split()
 MELCOW_NEW_USERS = is_enabled((getenv('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((getenv('PROTECT_CONTENT', "True")), False)
 PUBLIC_FILE_STORE = is_enabled((getenv('PUBLIC_FILE_STORE', "False")), False)
