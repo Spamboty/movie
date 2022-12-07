eee5e2bec5fabebfa0606054a1121fa6f7837551
Spamboty <119392303+Spamboty@users.noreply.github.com>
2022-12-07 20:07:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,9 +21,9 @@ PICS = (getenv('PICS', 'https://telegra.ph//file/b12246b0780a927bc8190.jpg https
 # Admins, Channels & Users
 ADMINS = getenv('ADMINS', '5463205082')
 CHANNELS = getenv('CHANNELS', ' -1001863651025')
-AUTH_USERS = int(getenv('AUTH_USERS', '5463205082').split())
-AUTH_CHANNEL = int(getenv('AUTH_CHANNEL', ' -1001863651025').split())
-AUTH_GROUP = int(getenv('AUTH_GROUP', '-1001666282080').split())
+AUTH_USERS = getenv('AUTH_USERS', '5463205082')
+AUTH_CHANNEL = getenv('AUTH_CHANNEL', ' -1001863651025')
+AUTH_GROUP = getenv('AUTH_GROUP', '-1001666282080')
 
 # MongoDB information
 DATABASE_URI = getenv('DATABASE_URI', "mongodb+srv://movies:7234049299@cluster0.mc1he3h.mongodb.net/?retryWrites=true&w=majority")
